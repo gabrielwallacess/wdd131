9eafe6296dc6981169d1bb5529870263bd185aee
gabrielwallacess <gsantos2018@gmail.com>
2026-07-10 22:46:59 -0300

    ok

--- scripts/templos.js
+++ scripts/templos.js
@@ -1,6 +1,14 @@
-const currentYear = new Date().getFullYear();
+const year = new Date().getFullYear();
 
-document.getElementById("currentyear").textContent = currentYear;
+document.getElementById("currentyear").textContent = year;
 
 document.getElementById("lastModified").textContent =
     `Última Modificação: ${document.lastModified}`;
+
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    menuButton.classList.toggle("open");
+});
