afb81b33f28cd9a39a4fd2139276c1f0e7cf0936
gabrielwallacess <gsantos2018@gmail.com>
2026-07-18 12:31:39 -0300

    ok

--- semana03/scripts/local.js
+++ semana03/scripts/local.js
@@ -1,44 +1,49 @@
-// Ano atual no rodapé
-const currentYear = new Date().getFullYear();
+const ano = new Date().getFullYear();
 
-document.querySelector("#currentyear").textContent = currentYear;
+document.querySelector("#currentyear").textContent = ano;
 
 
-// Última modificação do documento
+
 document.querySelector("#lastModified").textContent =
-    `Última atualização: ${document.lastModified}`;
+`Última atualização: ${document.lastModified}`;
 
 
 
-// Valores estáticos do clima
-const temperatura = 24; // °C
-const velocidadeVento = 8; // km/h
+const temperatura = 5;
+
+const velocidadeVento = 20;
 
 
 
-// Função para calcular sensação térmica
-function calcularSensacaoTermica(temperatura, velocidadeVento) {
+function calcularSensacaoTermica(temperatura, velocidadeVento){
 
-    return 13.12 + 0.6215 * temperatura 
-        - 11.37 * Math.pow(velocidadeVento, 0.16)
-        + 0.3965 * temperatura * Math.pow(velocidadeVento, 0.16);
+return 13.12 + 
+0.6215 * temperatura -
+11.37 * Math.pow(velocidadeVento,0.16) +
+0.3965 * temperatura *
+Math.pow(velocidadeVento,0.16);
+
+}
+
+
+
+let resultado = "N/A";
+
+
+
+if(temperatura <= 10 && velocidadeVento > 4.8){
+
+
+resultado =
+calcularSensacaoTermica(
+temperatura,
+velocidadeVento
+).toFixed(0)
++"°C";
+
 
 }
 
 
 
-// Verificação das condições para cálculo
-let sensacao = "N/A";
-
-if (temperatura <= 10 && velocidadeVento > 4.8) {
-
-    sensacao = calcularSensacaoTermica(
-        temperatura,
-        velocidadeVento
-    ).toFixed(0) + "°C";
-
-}
-
-
-// Exibir resultado na página
-document.querySelector("#windChill").textContent = sensacao;
+document.querySelector("#windChill").textContent = resultado;
